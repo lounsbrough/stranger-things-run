4c69bd8d4f6365bf9e3989ef9a05f6cd9a97bba3
David Lounsbrough <dlounsbrough@hy-vee.com>
2021-06-09 22:35:21 -0500

    socket.io code for stranger things

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+
+const port = 7241;
+
+const io = new Server(server, {
+    cors: {
+        origin: [
+            "http://localhost:3000",
+            "https://codenough.com",
+            "https://lounsbrough.com"
+        ]
+    }
+});
+
+const letterStates = 'abcdefghijklmnopqrstuvwxyz'.split('').map((letter) => ({
+    letter,
+    on: false
+}));
+
+io.on('connection', (socket) => {
+    console.log('client connected');
+
+    socket.on('join-room', (roomId) => {
+        console.log(`joined room ${roomId}`);
+
+        socket.join(roomId);
+    });
+
+    socket.on('light-state-change', (letterStateChange) => {
+        console.log(letterStateChange);
+
+        letterStates.find((letterState) => letterState.letter === letterStateChange.letter).on = letterStateChange.on;
+
+        console.log(socket.rooms);
+
+        socket.rooms.forEach(roomId => {
+            socket.to(roomId).emit('letter-state-change', letterStates);
+        });
+    });
+});
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
